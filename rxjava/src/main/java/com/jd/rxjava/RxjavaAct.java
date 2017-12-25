0afa69645e4281a32a5203ab5a08379fb6d91b55
yidingyuan <yidingyuanok@163.com>
2017-12-25 21:35:32 +0800

    copy

--- rxjava/src/main/java/com/jd/rxjava/RxjavaAct.java
+++ rxjava/src/main/java/com/jd/rxjava/RxjavaAct.java
@@ -9,8 +9,7 @@ import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
-
-
+import io.reactivex.functions.Function;
 
 
 public class RxjavaAct extends Activity {
@@ -22,13 +21,55 @@ public class RxjavaAct extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        test();
+        test1();
+        test2();
+
+
+    }
+
+    private void test2() {
+        final Observable<String> testCreateObservable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                e.onNext("1");
+                e.onComplete();
+            }
+        });
+
+        Observable<Integer> map = testCreateObservable.map(new Function<String, Integer>() {
+            @Override
+            public Integer apply(String s) throws Exception {
+                return Integer.parseInt(s);
+            }
+        });
+        
+        map.subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe() called with: d = [" + d + "]");
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d(TAG, "onNext() called with: value = [" + value + "]");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError() called with: e = [" + e + "]");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete() called");
+            }
+        });
 
 
     }
 
 
-    private void test() {
+    private void test1() {
 
         Observable.create(new ObservableOnSubscribe<String>() {
             @Override
